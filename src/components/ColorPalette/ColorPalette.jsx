9a3a20551480391e579eac8a78ad106d7f3dd200
Shubham <shubhamsinghmau2@gmail.com>
2024-06-11 14:49:16 +0530

    active color indicator

--- src/components/ColorPalette/ColorPalette.jsx
+++ src/components/ColorPalette/ColorPalette.jsx
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ColorContext } from '../../context/colorContext.jsx';
 import styles from './ColorPalette.module.css';
 
 function ColorPalette() {
-  const { setColorName } = useContext(ColorContext);
+  const { setColorName, colorName } = useContext(ColorContext);
+  const [activeColor, setActiveColor] = useState(colorName);
+
+  const handleColorSelect = (color) => {
+    setColorName(color);
+    setActiveColor(color);
+  }
+
 
   return (
     <div className={styles.container}>
-      <div className={`${styles.circle} ${styles.red}`} onClick={() => setColorName('red')}></div>
-      <div className={`${styles.circle} ${styles.green}`} onClick={() => setColorName('green')}></div>
-      <div className={`${styles.circle} ${styles.white}`} onClick={() => setColorName('white')}></div>
-      <div className={`${styles.circle} ${styles.black}`} onClick={() => setColorName('black')}></div>
+      <div className={`${styles.circle} ${styles.red} ${activeColor === 'red' ? styles.active : ''}`} onClick={() => handleColorSelect('red')}></div>
+      <div className={`${styles.circle} ${styles.green} ${activeColor === 'green' ? styles.active : ''}`} onClick={() => handleColorSelect('green')}></div>
+      <div className={`${styles.circle} ${styles.white} ${activeColor === 'white' ? styles.active : ''}`} onClick={() => handleColorSelect('white')}></div>
+      <div className={`${styles.circle} ${styles.black} ${activeColor === 'black' ? styles.active : ''}`} onClick={() => handleColorSelect('black')}></div>
     </div>
   );
 }

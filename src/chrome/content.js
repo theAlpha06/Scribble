fea17b71caadebdbd98fdd91a060a15e07cae2db
Shubham <shubhamsinghmau2@gmail.com>
2024-06-13 19:19:14 +0530

    fix: canvas going out of the viewport width

--- src/chrome/content.js
+++ src/chrome/content.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Popup } from '../popup.jsx';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Popup } from "../popup.jsx";
+import "./styles.css";
 
 const initializeScribble = () => {
-  const containerDiv = document.createElement('div');
-  containerDiv.id = 'scrible-root-container';
-  containerDiv.classList.add('scribble-root-container');
+  const containerDiv = document.createElement("div");
+  containerDiv.id = "scrible-root-container";
+  containerDiv.classList.add("scribble-root-container");
   document.body.appendChild(containerDiv);
 
-  const canvasDiv = document.createElement('canvas');
-  canvasDiv.id = 'scrible-root-container_canvas';
-  canvasDiv.width = window.innerWidth;
-  canvasDiv.height = window.innerHeight;
+  const canvasDiv = document.createElement("canvas");
+  canvasDiv.id = "scrible-root-container_canvas";
+  canvasDiv.width = document.body.clientWidth;
+  canvasDiv.height = document.body.clientHeight;
   document.body.insertBefore(canvasDiv, document.body.firstChild);
 
   let isDragging = false;
@@ -24,14 +24,14 @@ const initializeScribble = () => {
     offsetY = event.clientY - containerDiv.getBoundingClientRect().top;
     offsetZ = event.clientX - containerDiv.getBoundingClientRect().right;
   }
-  
+
   function handleMouseMove(event) {
     if (!isDragging) return;
-    
+
     const newLeft = event.clientX - offsetX;
     const newTop = event.clientY - offsetY;
     const newRight = event.clientX - offsetZ;
-    
+
     containerDiv.style.left = `${newLeft}px`;
     containerDiv.style.top = `${newTop}px`;
     containerDiv.style.width = `${newRight - newLeft}px`;
@@ -41,14 +41,14 @@ const initializeScribble = () => {
     isDragging = false;
   }
 
-  document.addEventListener('mousemove', handleMouseMove);
-  document.addEventListener('mouseup', handleMouseUp);
+  document.addEventListener("mousemove", handleMouseMove);
+  document.addEventListener("mouseup", handleMouseUp);
 
-  containerDiv.addEventListener('mousedown', handleMouseDown);
+  containerDiv.addEventListener("mousedown", handleMouseDown);
 
   ReactDOM.render(<Popup />, containerDiv);
 };
 
-if (!document.getElementById('scrible-root-container')) {
+if (!document.getElementById("scrible-root-container")) {
   initializeScribble();
 }
